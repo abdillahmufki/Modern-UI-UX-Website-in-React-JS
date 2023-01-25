7d0efcfcac48ce7b2e5e551c7b3740cb4f39da66
Abdillah Mufki Auzan Mubin <93027141+abdillahmufki@users.noreply.github.com>
2023-01-25 22:01:07 +0700

    header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./header.css";
+import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
 
 const Header = () => {
   return (
@@ -8,6 +10,26 @@ const Header = () => {
         <h1 className="gradient__text">
           Let’s Build Something amazing with GPT-3 OpenAI
         </h1>
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
+
+        <div className="whatblack__header-content__input">
+          <input type="email" placeholder="Your Email Address" />
+          <button type="button">Get Started</button>
+        </div>
+
+        <div className="whatblack__header-content__people">
+          <img src={people} alt="people" />
+          <p>1,600 people requested access a visit in last 24 hours</p>
+        </div>
+      </div>
+
+      <div className="whatblack__header-image">
+        <img src={ai} alt="ai" />
       </div>
     </div>
   );
