a74c805d8512972a5ca699562a5ae7b3ad4110c5
Abdillah Mufki Auzan Mubin <93027141+abdillahmufki@users.noreply.github.com>
2023-01-25 16:39:11 +0700

    .

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -2,7 +2,15 @@ import React from "react";
 import "./header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="whatblack__header section__padding" id="home">
+      <div className="whatblack__header-content">
+        <h1 className="gradient__text">
+          Let’s Build Something amazing with GPT-3 OpenAI
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
