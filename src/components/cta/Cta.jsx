45068bff38ff186e6d681461d674c8c00dae976b
Abdillah Mufki Auzan Mubin <93027141+abdillahmufki@users.noreply.github.com>
2023-01-27 22:37:30 +0700

    update day 3

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -2,7 +2,17 @@ import React from "react";
 import "./cta.css";
 
 function Cta() {
-  return <div>Cta</div>;
+  return (
+    <div className="whatblack__cta">
+      <div className="whatblack__cta-content">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register today & start exploring the endless possiblities.</h3>
+      </div>
+      <div className="whatblack__cta-btn">
+        <button type="button">Get Started</button>
+      </div>
+    </div>
+  );
 }
 
 export default Cta;
--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -3,11 +3,11 @@ import possibilityImage from "../../assets/possibility.png";
 import "./possibility.css";
 
 const Possibility = () => (
-  <div className="gpt3__possibility section__padding" id="possibility">
-    <div className="gpt3__possibility-image">
+  <div className="whatblack__possibility section__padding" id="possibility">
+    <div className="whatblack__possibility-image">
       <img src={possibilityImage} alt="possibility" />
     </div>
-    <div className="gpt3__possibility-content">
+    <div className="whatblack__possibility-content">
       <h4>Request Early Access to Get Started</h4>
       <h1 className="gradient__text">
         The possibilities are <br /> beyond your imagination
