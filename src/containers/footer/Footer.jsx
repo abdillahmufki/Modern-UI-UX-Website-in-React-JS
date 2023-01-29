7d7b4c83f624e540a3daed53367a65bb1e2a4077
Abdillah Mufki Auzan Mubin <93027141+abdillahmufki@users.noreply.github.com>
2023-01-29 22:32:10 +0700

    final commit

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,8 +1,54 @@
 import React from "react";
 import "./footer.css";
+import whatblackLogo from "../../assets/logo.svg";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="whatblack__footer section__padding">
+      <div className="whatblack__footer-heading">
+        <h1 className="gradient__text">
+          Do you want to step in to the future before others
+        </h1>
+      </div>
+
+      <div className="whatblack__footer-btn">
+        <p>Request Early Access</p>
+      </div>
+
+      <div className="whatblack__footer-links">
+        <div className="whatblack__footer-links_logo">
+          <img src={whatblackLogo} alt="logo images" />
+          <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
+        </div>
+
+        <div className="whatblack__footer-links_div">
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+
+        <div className="whatblack__footer-links_div">
+          <h4>Company</h4>
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+
+        <div className="whatblack__footer-links_div">
+          <h4>Get in touch</h4>
+          <p>Crechterwoord K12 182 DK Alknjkcb</p>
+          <p>085-132567</p>
+          <p>info@payme.net</p>
+        </div>
+      </div>
+
+      <div className="whatblack__footer-copyright">
+        <p>© 2021 GPT-3. All rights reserved.</p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
